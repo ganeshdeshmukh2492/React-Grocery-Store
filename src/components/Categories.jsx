91c029964ca2a2a63457dca9aedc0f6a37750b27
ganeshdeshmukh2492 <deshmukhganesh2492@gmail.com>
2026-05-13 16:07:38 +0530

    newchange

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -21,31 +21,31 @@ const Categories = () => {
   },
    {
     id: 3,
-    title: "Milk",
-    price: "₹60/litre",
-    category: "Dairy",
-    image: "https://images.unsplash.com/photo-1771503937636-c0e37bd23517?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    title: "Banana",
+    price: "₹60",
+    category: "Fruits",
+    image: "https://media.istockphoto.com/id/2237270821/photo/young-woman-buys-fresh-fruits-and-vegetables-at-the-market.webp?a=1&b=1&s=612x612&w=0&k=20&c=jzDjHU32G-j1GsBF-GSFdAQKBnrojJyhh8wcCPTyBxk="
   },
    {
     id: 4,
-    title: "Milk",
-    price: "₹60/litre",
-    category: "Dairy",
-    image: "https://images.unsplash.com/photo-1771503937636-c0e37bd23517?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    title: "Grapes",
+    price: "₹60/kg",
+    category: "Fruits",
+    image: "https://images.unsplash.com/photo-1580451359753-6f163ed6c244?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
    {
     id: 5,
-    title: "Milk",
-    price: "₹60/litre",
-    category: "Dairy",
-    image: "https://images.unsplash.com/photo-1771503937636-c0e37bd23517?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    title: "Potato",
+    price: "₹20/kg",
+    category: "Vegetables",
+    image: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
    {
     id: 6,
-    title: "Milk",
-    price: "₹60/litre",
-    category: "Dairy",
-    image: "https://images.unsplash.com/photo-1771503937636-c0e37bd23517?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    title: "Spinach",
+    price: "₹20",
+    category: "Vegetables",
+    image: "https://images.unsplash.com/photo-1576045057995-568f588f82fb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpbmFjaHxlbnwwfHwwfHx8MA%3D%3D"
   },
 
 ];
